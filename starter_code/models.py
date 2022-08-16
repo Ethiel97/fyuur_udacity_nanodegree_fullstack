d266aae52aef079003d74ff3eec8606fd0b8ce48
Ethiel ADIASSA <ethiel97@gmail.com>
2022-08-16 14:40:23 +0000

    First commit

---
+++ starter_code/models.py
@@ -0,0 +1,59 @@
+from app import db
+from sqlalchemy.sql import func
+
+
+class Venue(db.Model):
+    __tablename__ = 'Venue'  # type: str
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    city = db.Column(db.String(120))
+    state = db.Column(db.String(120))
+    address = db.Column(db.String(120))
+    phone = db.Column(db.String(120))
+    image_link = db.Column(db.String(500))
+    website_link = db.Column(db.String(120))
+    facebook_link = db.Column(db.String(120))
+    seeking_talent = db.Column(db.Boolean, default=False)
+    seeking_description = db.Column(db.String(500))
+    shows = db.relationship('Show', backref='venue', lazy=True)
+    genres = db.Column(db.String(120))
+
+    def __repr__(self) -> str:
+        return f"<Venue id: {self.id}, name: {self.name}>"
+
+    # TODO: implement any missing fields, as a database migration using Flask-Migrate
+
+
+class Show(db.Model):
+    __tablename__ = 'Show'
+
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    start_time = db.Column(db.DateTime(timezone=True), default=func.now())
+    artist_id = db.Column(db.Integer, db.ForeignKey(
+        'Artist.id'), nullable=False)
+    venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'), nullable=False)
+
+    def __repr__(self) -> str:
+        return "<Show id: {self.id}, name: {self.artist_name}>"
+
+
+class Artist(db.Model):
+    __tablename__ = 'Artist'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    city = db.Column(db.String(120))
+    state = db.Column(db.String(120))
+    phone = db.Column(db.String(120))
+    genres = db.Column(db.String(120))
+    image_link = db.Column(db.String(500))
+    facebook_link = db.Column(db.String(120))
+    website_link = db.Column(db.String(120))
+    seeking_venue = db.Column(db.Boolean, default=False)
+    seeking_description = db.Column(db.String(500))
+    shows = db.relationship('Show', backref='artist', lazy=True)
+
+    def __repr__(self) -> str:
+        return f"<Artist id: {self.id}, name: {self.name}>"
